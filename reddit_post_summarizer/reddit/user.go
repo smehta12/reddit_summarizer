40cd20b99539577f891106f941b43bd02cad082d
smehta12 <shalinmehta85@gmail.com>
2023-04-13 08:30:31 -0700

    Fixed unnsuported_grant_type error

--- reddit_post_summarizer/reddit/user.go
+++ reddit_post_summarizer/reddit/user.go
@@ -1,41 +1,24 @@
 package reddit
 
 import (
-	"bytes"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
 
 // TODO: getting {"error": "unsupported_grant_type"} when running code below
 func GetUserToken(username string, password string) string {
-	type User struct {
-		Username  string `json:"username"`
-		Password  string `json:"password"`
-		GrantType string `json:"grant_type"`
-	}
+	data := url.Values{}
+	data.Set("username", username)
+	data.Set("password", password)
+	data.Set("grant_type", "password")
+	encodedData := data.Encode()
 
-	u := User{
-		Username:  username,
-		Password:  password,
-		GrantType: "password",
-	}
-
-	var buf bytes.Buffer
-	err := json.NewEncoder(&buf).Encode(u)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// var jsonData = []byte(`{
-	// 	"username": "appsummrize",
-	// 	"password": "app123**"
-	// 	"grant_type": "password"
-	// }`)
-
-	r, err := http.NewRequest("POST", "https://www.reddit.com/api/v1/access_token", &buf) //bytes.NewBuffer(jsonData)) //&buf)
+	r, err := http.NewRequest("POST", "https://www.reddit.com/api/v1/access_token", strings.NewReader(encodedData))
 
 	if err != nil {
 		panic(err)
@@ -44,13 +27,7 @@ func GetUserToken(username string, password string) string {
 	r.Header.Add("User-Agent", "posts_summarizer/0.0.1")
 	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-	app_id := "ot7-YcZcb5b8DzATpM9QPg"
-	secret := "0FMS283Nyzb32Csmb05FRsgoaXKftg"
-	// auth := app_id + ":" + secret
-	// encodedAuth := base64.StdEncoding.EncodeToString([]byte(auth))
-	// r.Header.Set("Authorization", "Basic "+encodedAuth)
-
-	r.SetBasicAuth(app_id, secret)
+	r.SetBasicAuth(os.Getenv("REDDIT_APP_ID"), os.Getenv("REDDIT_SECRET"))
 
 	client := &http.Client{}
 
@@ -68,7 +45,13 @@ func GetUserToken(username string, password string) string {
 		panic(err)
 	}
 	log.Println("Response Status:", res.Status)
-	log.Println(string(responseData))
 
-	return string(responseData)
+	m := make(map[string]string)
+	err = json.Unmarshal(responseData, &m)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return m["access_token"]
 }
