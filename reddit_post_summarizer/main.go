c596ca002959ca0f2cda31ee1a73eccb6f1c2e93
smehta12 <shalinmehta85@gmail.com>
2023-04-13 08:29:32 -0700

    Moved uname, pwd to env var; Added call to text summarization.

--- reddit_post_summarizer/main.go
+++ reddit_post_summarizer/main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"os"
 
+	"./inference" //TODO: Change to proper package name
 	"./reddit"
 )
 
 func main() {
-	bearerToken := "22403373384748-__MNvQmkd1ZdSsnQnzVNEpOauH9mDg" //reddit.GetUserToken("appsummrize", "app123**")
+	bearerToken := reddit.GetUserToken(os.Getenv("USERNAME"), os.Getenv("PASSWORD"))
 	subredditName := "personalfinance"
-	postId := "11yqgoo"
+	postId := "12bpmx3"
 	sortingMethod := "top"
 	depth := 1
 
@@ -27,5 +28,5 @@ func main() {
 		f.WriteString(val)
 		// f.WriteString("\n--------\n")
 	}
-
+	inference.GetSummarizedText(comments)
 }
