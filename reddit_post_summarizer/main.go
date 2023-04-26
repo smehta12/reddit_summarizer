f852684a96e37b53184137e1879b8866fbdd8d51
smehta12 <shalinmehta85@gmail.com>
2023-04-26 08:27:47 -0700

    Added web interface for getting summary

--- reddit_post_summarizer/main.go
+++ reddit_post_summarizer/main.go
@@ -1,35 +1,74 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"net/url"
+	"strings"
 
-	"github.com/smehta12/reddit_summarizer/inference" //TODO: Change to proper package name
+	"github.com/gin-gonic/gin"
+	"github.com/smehta12/reddit_summarizer/inference"
 	"github.com/smehta12/reddit_summarizer/reddit"
 )
 
-func main() {
-	bearerToken := reddit.GetUserToken(os.Getenv("USERNAME"), os.Getenv("PASSWORD"))
-	subredditName := "personalfinance"
-	postId := "12bpmx3"
+//TODO: Change to proper package name
+
+func getSummary(c *gin.Context) {
+	var subredditName string
+	var postId string
+
+	redditURL := c.Query("reddit_url")
+	// TODO: Get from auth method
+	redditUName := c.Query("reddit_username")
+	redditPwd := c.Query("reddit_password")
+
+	if redditURL == "" || redditUName == "" || redditPwd == "" {
+		c.JSON(400, gin.H{"error": "missing parameters"})
+	}
+
+	parsedUrl, err := url.Parse(redditURL)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "invalid Reddit URL"})
+		return
+	}
+
+	uri := parsedUrl.Path
+	splittedURI := strings.Split(uri, "/")
+
+	if splittedURI[1] != "r" {
+		c.JSON(400, gin.H{"error": "invalid Reddit URL"})
+		return
+	} else {
+		subredditName = splittedURI[2]
+	}
+
+	if splittedURI[3] != "comments" {
+		c.JSON(400, gin.H{"error": "invalid Reddit URL"})
+		return
+	} else {
+		postId = splittedURI[4]
+	}
+
 	sortingMethod := "top"
 	depth := 1
 
+	bearerToken := reddit.GetUserToken(redditUName, redditPwd)
 	comments := reddit.LoadComments(subredditName, postId, sortingMethod, depth, bearerToken)
-
-	f, err := os.Create("comments.txt")
-	if err != nil {
-		fmt.Println("Issue in file creation")
-	}
-
-	defer f.Close()
-
-	for _, val := range comments {
-		f.WriteString(val)
-		// f.WriteString("\n--------\n")
-	}
-
 	summarizedText := inference.GetSummarizedText(comments)
 
-	print("Got Below Summarized Text: \n" + summarizedText)
+	c.JSON(200, strings.TrimSpace(summarizedText))
+}
+
+func main() {
+
+	// Generate gin code endpoint for post request
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Reddit Summarizer",
+		})
+	})
+	router.GET("/summary", getSummary)
+
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
