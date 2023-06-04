ed2e819cf00a6a84ed70d651ba7499a3c7c99cda
smehta12 <shalinmehta85@gmail.com>
2023-06-04 09:34:13 -0700

    Getting yaml config. Uses common module for getting summarized text

--- reddit_post_summarizer/main.go
+++ reddit_post_summarizer/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/url"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/smehta12/reddit_summarizer/inference"
 	"github.com/smehta12/reddit_summarizer/reddit"
+	"gopkg.in/yaml.v3"
 )
 
 //TODO: Change to proper package name
@@ -50,11 +52,42 @@ func getSummary(c *gin.Context) {
 	sortingMethod := "top"
 	depth := 1
 
+	// TODO: Add parallel summarization from different algorithms. Loop over different algorithms and
+	// get the best one.
 	bearerToken := reddit.GetUserToken(redditUName, redditPwd)
 	comments := reddit.LoadComments(subredditName, postId, sortingMethod, depth, bearerToken)
-	summarizedText := inference.GetSummarizedText(comments)
+
+	var sr inference.SummarizerRequester
+	ors := inference.OpenAIRequestSummary{Paragraph: ""}
+	sr = &ors
+
+	config := getYamlConfig()
+	con, ok := config["text-davinci-003"].(map[string]interface{})
+	if !ok {
+		panic("could not find key in the model config")
+	}
+	summarysize := con["summary_size"].(int)
+	totalMaxTokens := con["max_tokens"].(int) - summarysize - len(con["summary_suffix"].(string))
+	summarizedText := inference.GetSummarizedText(sr, comments, summarysize, totalMaxTokens)
 
 	c.JSON(200, strings.TrimSpace(summarizedText))
+}
+
+func getYamlConfig() map[string]interface{} {
+
+	yamlFile, err := ioutil.ReadFile("../model_configs.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	// Unmarshal the YAML file into a map.
+	config := make(map[string]interface{})
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	return config
 }
 
 func main() {
@@ -68,6 +101,7 @@ func main() {
 			"message": "Reddit Summarizer",
 		})
 	})
+
 	router.GET("/summary", getSummary)
 
 	router.Run() // listen and serve on 0.0.0.0:8080
