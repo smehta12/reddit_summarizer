9cbb99616c40c24f0d773b0e537da1aa98970091
smehta12 <shalinmehta85@gmail.com>
2023-06-11 16:57:41 -0700

    Passing model names to summarizers

--- reddit_post_summarizer/inference/summarizer.go
+++ reddit_post_summarizer/inference/summarizer.go
@@ -1,7 +1,11 @@
 package inference
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"log"
+	"net/http"
 	"strings"
 
 	"github.com/pkoukk/tiktoken-go"
@@ -13,11 +17,13 @@ type SummarizerRequester interface {
 
 type PyServiceRequestSummary struct {
 	Paragraph *string
+	ModelName string
 }
 
-func GetSummarizedText(sr SummarizerRequester, comments []string, summarySize int, totalMaxTokens int) string {
+func GetSummarizedText(sr SummarizerRequester, comments []string, summarySize int, totalMaxTokens int,
+	model_name string) string {
 	cleanupComments(comments)
-	summarizedText := summarizeTextRecursive(sr, comments, summarySize, totalMaxTokens)
+	summarizedText := summarizeTextRecursive(sr, comments, summarySize, totalMaxTokens, model_name)
 	summarizedText = formatSummary(&summarizedText)
 	return summarizedText
 }
@@ -25,7 +31,8 @@ func GetSummarizedText(sr SummarizerRequester, comments []string, summarySize in
 // comments: data to summarize
 // summarySize: How long summary can be.
 // totalTokens: Total Max tokens. e.g. 4096 in text-davinci-003
-func summarizeTextRecursive(sr SummarizerRequester, comments []string, summarySize int, totalMaxTokens int) string {
+func summarizeTextRecursive(sr SummarizerRequester, comments []string, summarySize int, totalMaxTokens int,
+	model_name string) string {
 	// base case.
 	if len(comments) == 1 && len(comments[0]) < summarySize {
 		return comments[0]
@@ -44,7 +51,7 @@ func summarizeTextRecursive(sr SummarizerRequester, comments []string, summarySi
 		if totalNumOfTokens <= totalMaxTokens {
 			paragraph += comments[i]
 		} else {
-			assignParagraph(sr, &paragraph)
+			assignParagraph(sr, &paragraph, model_name)
 			summarizedText = append(summarizedText, sr.requestSummary())
 			i--
 			totalNumOfTokens = 0
@@ -54,17 +61,19 @@ func summarizeTextRecursive(sr SummarizerRequester, comments []string, summarySi
 	}
 
 	// for last paragraph
-	assignParagraph(sr, &paragraph)
+	assignParagraph(sr, &paragraph, model_name)
 	summarizedText = append(summarizedText, sr.requestSummary())
-	return summarizeTextRecursive(sr, summarizedText, summarySize, totalMaxTokens)
+	return summarizeTextRecursive(sr, summarizedText, summarySize, totalMaxTokens, model_name)
 }
 
-func assignParagraph(sr SummarizerRequester, paragraph *string) {
+func assignParagraph(sr SummarizerRequester, paragraph *string, model_name string) {
 	switch s := sr.(type) {
 	case *PyServiceRequestSummary:
 		s.Paragraph = paragraph
+		s.ModelName = model_name
 	case *OpenAIRequestSummary:
 		s.Paragraph = paragraph
+		s.ModelName = model_name
 	}
 }
 
@@ -86,5 +95,23 @@ func getNumberOfTokens(comment string) int {
 }
 
 func (rs PyServiceRequestSummary) requestSummary() string {
-	return ""
+	values := map[string]string{"model_name": rs.ModelName, "prompt": *rs.Paragraph}
+
+	jsonValue, _ := json.Marshal(values)
+
+	resp, err := http.Post("http://0.0.0.0:8000/summarize", "application/json", bytes.NewBuffer(jsonValue))
+
+	if err != nil {
+		log.Println("Error when requsting summary from python service for model" + rs.ModelName)
+	}
+
+	// fmt.Println(resp)
+	respData, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Println("Error in reading from response for" + rs.ModelName)
+		panic(err)
+	}
+
+	return string(respData)
 }
