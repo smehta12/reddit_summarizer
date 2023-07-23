6625e6e8d101ee399395f6929f9ccbdfd2c21dcc
smehta12 <shalinmehta85@gmail.com>
2023-07-23 09:49:06 -0700

    Added module to run gunicorn manually.

---
+++ python_service/gunicorn_run.py
@@ -0,0 +1,31 @@
+import multiprocessing
+
+from gunicorn.app.wsgiapp import WSGIApplication
+
+
+class StandaloneApplication(WSGIApplication):
+    def __init__(self, app_uri, options=None):
+        self.options = options or {}
+        self.app_uri = app_uri
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+
+def run():
+    options = {
+        "bind": "0.0.0.0:8000",
+        "workers": (multiprocessing.cpu_count() * 2) + 1,
+        "worker_class": "uvicorn.workers.UvicornWorker",
+        "timeout": 1200 # TODO: Reduce timeouts
+    }
+    StandaloneApplication("python_service.backend.main:app", options).run()
+
+run()
