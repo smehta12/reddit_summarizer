e55ff43629230ad817a8ce5461c9055f6a9dc1b0
smehta12 <shalinmehta85@gmail.com>
2023-07-23 09:50:24 -0700

    Fixed path to saved model file.

--- python_service/summarizers/huggingface.py
+++ python_service/summarizers/huggingface.py
@@ -2,15 +2,18 @@ import os
 from python_service.summarizers.summarizer_base import SummarizerBase
 from transformers import TFAutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 
-MODEL_CACHE_PATH = "../../model_cache"
+MODEL_CACHE_PATH = f"{os.path.dirname(os.path.abspath(__file__))}/../../model_cache"
 
 class HuggingFaceSummarizer(SummarizerBase):
     def __init__(self, model_name):
         super().__init__(model_name)
         model_name = self.model_configs["model_name"]
         tokenizer_name = self.model_configs["tokenizer_name"]
+        saved_model_filename = self.model_configs["saved_model_file_name"]
 
-        if not os.path.exists(f"{MODEL_CACHE_PATH}/{model_name}/{model_name}.h5"):
+        model_path = f"{MODEL_CACHE_PATH}/{model_name}/{saved_model_filename}"
+
+        if not os.path.exists(model_path):
             self.model = TFAutoModelForSeq2SeqLM.from_pretrained(model_name)
             self.model.save_pretrained(f"{MODEL_CACHE_PATH}/{model_name}")
             self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
